b09eeedcc8d515ecc7b46123df4c65317f6c5748
kjh99660 <kdh4021200@naver.com>
2020-02-16 22:58:06 +0900

    2020.02.16 작업분 설명은 README

---
+++ writer_project_unity/Assets/Script/Exit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Exitgame()
+    {
+        Application.Quit();
+        Debug.Log("game exit");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
