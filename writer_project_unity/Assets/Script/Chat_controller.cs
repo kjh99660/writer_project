b0d1618e7b84569288be03e6754c843115f5e7a7
kdh4021200 <kjh996601020@gmail.com>
2020-03-27 21:09:19 +0900

    스크립트 재생 함수 수정 및 변수 이름 수정

--- writer_project_unity/Assets/Script/Chat_controller.cs
+++ writer_project_unity/Assets/Script/Chat_controller.cs
@@ -6,23 +6,22 @@ using UnityEngine.UI;
 public class Chat_controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Text Name;
-    public Text Text;
-    public GameObject Next_button;
+    public Text NameS1;
+    public Text TextS1;
     private bool Click = true; 
 
     public void Click_Text()
     {
         Click = false;
     }
-    IEnumerator Chatting(string narrator, string narration)
+    IEnumerator Chatting(Text text ,string narrator, string narration)
     {
-        Name.text = narrator;
+        NameS1.text = narrator;
         string writertext = "";
         for(int i=0; i< narration.Length;i++)
         {
             writertext += narration[i];
-            Text.text = writertext;
+            text.text = writertext;
             yield return null;
         }
     }//한 프레임마다 한 글자씩 생성
@@ -37,10 +36,10 @@ public class Chat_controller : MonoBehaviour
     IEnumerator Texting()
     {//대사 출력하는 곳
         yield return StartCoroutine(Next());
-        yield return StartCoroutine(Chatting("주인공", "대사 출력 구현하기 연습 중 입니다"));
+        yield return StartCoroutine(Chatting(TextS1, "주인공", "대사 출력 구현하기 연습 중 입니다"));
 
         yield return StartCoroutine(Next());
-        yield return StartCoroutine(Chatting("주인공","안녕하세요?"));
+        yield return StartCoroutine(Chatting(TextS1, "주인공", "안녕하세요?"));
 
         yield return StartCoroutine(Next());
 
