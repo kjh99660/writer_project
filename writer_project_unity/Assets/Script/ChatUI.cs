27c46628f733ca4fcdd5e5bad64a2faa83ff4c35
kdh4021200 <kjh996601020@gmail.com>
2020-08-19 20:39:11 +0900

    UI 텍스트 창 수납 기능 추가

---
+++ writer_project_unity/Assets/Script/ChatUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatUI : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+    bool isClose = false;
+
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = transform.position;
+        targetPos.y -= 3.2f;
+    }
+    public void Changeform()
+    {
+        if (isClose) isClose = false;
+        else isClose = true;
+    }
+
+    void Update()
+    {
+        if(isClose)
+        {
+            Vector3 velo = Vector3.zero;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velo, 0.1f);
+        }
+        else
+        {
+            Vector3 velo = Vector3.zero;
+            transform.position = Vector3.SmoothDamp(transform.position, startPos, ref velo, 0.1f);
+        }
+
+
+
+    }
+}
